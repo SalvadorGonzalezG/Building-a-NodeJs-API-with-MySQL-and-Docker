4a3fbfe6379d26e83c22965df110827c2bb23a16
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-02-07 14:44:50 -0600

    adding interface, patient.js file & adding a fully complete C.R.U.D to patient.controller.js file

---
+++ interface/patient.js
@@ -0,0 +1,15 @@
+
+    // Objeto Patient con varias propiedades donde cada propiedad tiene un tipo ""
+const Patient = {
+    id: Number,
+    first_name: "",
+    last_name: "",
+    email: "",
+    address: "",
+    diagnosis: "",
+    phone: "",
+    status: "",
+    image_url: "",
+}
+    // Exportamos el objeto para que pueda ser utilizado en otros archivos.
+module.exports = Patient;

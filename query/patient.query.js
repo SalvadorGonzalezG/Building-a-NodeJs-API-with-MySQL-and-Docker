688b712787179b448fea9c7a944e1c2e0312abb5
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-02-06 23:39:30 -0600

    Onjeto, contiene diferentes consultas SQL utilizadas para interactuar con una DB la cual tiene una tabla llamada patients

---
+++ query/patient.query.js
@@ -0,0 +1,13 @@
+
+//Objeto llamado QUERY, el cual contiene diferentes consultas SQL, utilizadas para interactuar con una tabla llamada patients en una DB.  
+export const QUERY = {
+    //Selec todos los pacientes ordenados por fecha de creacion de forma DECENDENTE y limitar el resultado a 50 registros.
+    SELECT_PATIENTS: 'SELECT * FROM patients ORDER BY created_at DESC LIMIT 50',
+    //Selec todos los datos de un paciente especifico basado en su ID.
+    SELECT_PATIENT: 'SELECT * FROM patients WHERE id = ?',
+    CREATE_PATIENT: 'INSERT INTO patients (first_name, last_name, email, address, diagnosis, phone, status, image_url) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+    //Actualizar los datos de un paciente existente en la tabla de pacientes con los campos proporcionados, basado en su ID.
+    UPDATE_PATIENT: 'UPDATE patients SET first_name = ?, last_name = ?, email = ?, address = ?, diagnosis = ?, phone = ?, status = ?, image_url = ? WHERE id = ?',
+    //Eliminar un paciente de la tabla pacientes basado en su ID.
+    DELETE_PATIENT: 'DELETE FROM patients WHERE id = ?'
+};

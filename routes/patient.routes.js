5d7567081cbded54146b27b84f9912eb0a7c6a93
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-02-07 16:43:28 -0600

    Definimos lo que realizara cada una de las rutas con los elementos que creamos en patient.controller.js

--- routes/patient.routes.js
+++ routes/patient.routes.js
@@ -1,6 +1,21 @@
 const Router = require("express")
+const { getPatients, getPatient, createPatient, updatePatient, deletePatient } = require('../controller/patient.controller')
 
 // Creando una nueva intancia del objeto Router 
 const patientRoutes = Router();
+// para la ruta raiz se manejan las operaciones GET
+patientRoutes.route('/')
+    // Obtener todos los pacientes
+    .get(getPatients)
+    // Crear un nuevo paciente.
+    .post(createPatient)
+
+patientRoutes.route('/:patientId')
+    // Obtener un paciente por su ID
+    .get(getPatient)
+    // Actualizar un paciente existente
+    .put(updatePatient)
+    // Eliminar un pacinete existente
+    .delete(deletePatient);
 
 module.exports = patientRoutes;
